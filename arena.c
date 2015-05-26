21d8abbdc739dca0b8fd39ebbfaff9065d74ed33
Jeff Larson <thejefflarson@gmail.com>
2015-05-26 15:13:36 -0400

    waypoint

--- arena.c
+++ arena.c
@@ -1,18 +1,28 @@
 #include "arena.h"
+#include <stdlib.h>
+
+#define PAGE_SIZE 4096
 
 arena_t *
 arena_create() {
-
+  arena_t *arena = (arena_t *)calloc(1, sizeof(arena));
+  if(!arena) return NULL;
+  arena->region = (uint8_t *)calloc(PAGE_SIZE, sizeof(uint8_t));
+  arena->size   = PAGE_SIZE;
+  if(!arena->region) {free(arena); return NULL; }
+  return arena;
 }
 
 void *
 arena_malloc(arena_t *arena, size_t size) {
-
-}
-
-void *
-arena_calloc(arena_t *arena, size_t num, size_t size) {
-
+  arena_t *last = arena;
+  do {
+    if((arena->size - arena->current) > size){
+      arena->current += size;
+      return arena->region + (arena->size - arena->current);
+    }
+    last = arena;
+  } while((arena = arena->next));
 }
 
 void
--- arena.h
+++ arena.h
@@ -16,9 +16,6 @@ arena_create();
 void *
 arena_malloc(arena_t *arena, size_t size);
 
-void *
-arena_calloc(arena_t *arena, size_t num, size_t size);
-
 void
 arena_destroy(arena_t *arena);
 
