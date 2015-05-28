7e922080aa032cf72425f72d60392ff78a9b3a13
Jeff Larson <thejefflarson@gmail.com>
2015-05-28 08:55:09 -0400

    better test

--- test.c
+++ test.c
@@ -4,9 +4,14 @@
 int
 main() {
   arena_t *arena = arena_create();
-  uint8_t *it = arena_malloc(arena, sizeof(uint8_t));
+  uint8_t *it = arena_malloc(arena, 1);
   *it = 7;
   printf("%i\n", *it);
+  for(int i; i < 10; i++) {
+  	uint8_t *i = arena_malloc(arena, 100);
+  	i[99] = 7;
+  }
+  it = arena_malloc(arena, 5000);
   arena_destroy(arena);
   return 0;
 }
